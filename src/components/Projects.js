6c583979b3bf5b756ab5e3083110ee900dc8b841
ayaaalli <ayaali95248@gmail.com>
2024-03-12 01:00:43 +0200

    ay7aga 3shan a2om anam

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -39,9 +39,14 @@ function Projects(){
                 <div class="card bg-dark text-white">
                         <img src={pic2} class="card-img" alt="..." height={'100%'}></img>
                         <div class="card-img-overlay">
-                            <p class="card-text">This is a wider card with suppo
-                            rting text below as a natural lead-in to additional content. 
-                            This content is a little bit longer.</p>
+                        <p class="card-text">
+                                <p className="Header">Ocean Box</p>
+                                <p>
+                                This website provides a comprehensive and user-friendly 
+                                homepage for a fish store. 
+                                </p>
+                                <button class="btn btn-primary seemorebtn" type="button" ><a href="https://ayaa1i.github.io/OceanBox/">see more <i class="fas fa-arrow-right"></i></a></button>
+                            </p>
                         </div>
                     </div>                
                 </div>
@@ -49,9 +54,15 @@ function Projects(){
                 <div class="card bg-dark text-white">
                         <img src={pic3} class="card-img" alt="..." height={'100%'}></img>
                         <div class="card-img-overlay">
-                            <p class="card-text">This is a wider card with suppo
-                            rting text below as a natural lead-in to additional content. 
-                            This content is a little bit longer.</p>
+                        <p class="card-text">
+                        <p className="Header">Toffee project</p>
+                                <p> 
+                                Project coded by java and clarified using UML designs 
+                                used CSV files to store data and used libraries to send a 
+                                real OTP to email.  
+                                </p>
+                                <button class="btn btn-primary seemorebtn" type="button" ><a href="https://github.com/mariamayman10/Toffee/tree/main">see more <i class="fas fa-arrow-right"></i></a></button>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -61,9 +72,14 @@ function Projects(){
                 <div class="card bg-dark text-white">
                         <img src={pic4} class="card-img" alt="..." height={'100%'}></img>
                         <div class="card-img-overlay">
-                            <p class="card-text">This is a wider card with suppo
-                            rting text below as a natural lead-in to additional content. 
-                            This content is a little bit longer.</p>
+                        <p class="card-text">
+                        <p className="Header">Orders</p>
+                                <p>
+                                This projects provides features to control the orders and 
+                                notifications  
+                                </p>
+                                <button class="btn btn-primary seemorebtn" type="button" ><a href="https://github.com/AyaA1i/Orders-and-Notifications-Management">see more <i class="fas fa-arrow-right"></i></a></button>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -71,9 +87,15 @@ function Projects(){
                 <div class="card bg-dark text-white">
                         <img src={pic5} class="card-img" alt="..." height={'100%'}></img>
                         <div class="card-img-overlay">
-                            <p class="card-text">This is a wider card with suppo
-                            rting text below as a natural lead-in to additional content. 
-                            This content is a little bit longer.</p>
+                        <p class="card-text">
+                                <p className="Header"> Student Affairs</p>
+                                <p>
+                               
+                                A website to add, edit, and delete students' data in the 
+                                college system. 
+                                </p>
+                                <button class="btn btn-primary seemorebtn" type="button" ><a href="https://github.com/AyaA1i/Web-Project-1">see more <i class="fas fa-arrow-right"></i></a></button>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -81,9 +103,14 @@ function Projects(){
                 <div class="card bg-dark text-white">
                         <img src={pic6} class="card-img" alt="..." height={'100%'}></img>
                         <div class="card-img-overlay">
-                            <p class="card-text">This is a wider card with suppo
-                            rting text below as a natural lead-in to additional content. 
-                            This content is a little bit longer.</p>
+                        <p class="card-text">
+                                <p className="Header">Library System </p>
+                                <p>
+                                A desktop application saves data in SQL tables and 
+                                connects with it by Java GUI. 
+                                </p>
+                                <button class="btn btn-primary seemorebtn" type="button" ><a href="https://github.com/AyaA1i/UniversityLibraryManagement">see more <i class="fas fa-arrow-right"></i></a></button>
+                            </p>
                         </div>
                     </div>
                 </div>
